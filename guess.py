01a5831c21d6b8c66beeccc80f621324d9d03868
m o n z o <creator.shminty@gmail.com>
2022-01-30 20:02:06 +0000

    rubbish guessing game ( ´･･)ﾉ(._.`)

---
+++ guess.py
@@ -0,0 +1,43 @@
+import random
+
+def game():
+
+    try:
+        numRange = int(input('highest number you would like to play up to: '))
+    except:
+        if type(numRange) != int:
+            print('please input a number!')
+            numRange = int(input())
+    
+    if numRange > 50:
+        print("let us not get ahead of ourselves\nsomething below 50\nsmh")
+        numRange = int(input('50 > : '))
+
+    number = random.randint(1,numRange)
+
+    guesses = (numRange // 2) + 1
+
+    guess = int(input('guess: '))
+    
+
+    while guess != number:
+        guesses -= 1
+        guess = int(input('guess: '))
+        if guesses == 0:
+            print('unfortunately, you have run out of guesses')
+            break
+
+
+    if guess == number or guesses == 0:
+        if guess == number:
+            print(f'correct! the number was {number}')
+        playAgain = input('would you like to play again (y/n): ')
+        if playAgain.lower() == 'y':
+            game()
+        else:
+            exit()
+        
+
+
+game()
+
