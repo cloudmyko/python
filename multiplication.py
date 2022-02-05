9990460e2bbd99425f2d9e396efd327422a28f3a
m o n z o <creator.shminty@gmail.com>
2022-02-05 20:58:35 +0000

    multiplication calculator thingy

---
+++ multiplication.py
@@ -0,0 +1,14 @@
+print('multiplication calculator')
+print('*************************')
+number1 = int(input('1: '))
+number2 = int(input('2: '))
+print('\n')
+print('*************************')
+
+
+def multiply(number1,number2):
+    return number1 * number2
+
+calc = multiply(number1,number2)
+
+print(f'the answer is: {calc}')
