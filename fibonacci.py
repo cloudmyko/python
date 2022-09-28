76006856327344863482ec4a6ab8ff69f63cc1b6
m o n z o <creator.shminty@gmail.com>
2022-09-28 22:15:50 +0100

    ..

--- fibonacci.py
+++ fibonacci.py
@@ -4,7 +4,7 @@ length = int(input())
 
 seq = [1,1]
 
-for i in range(length - 1):
+for i in range(length - 2):
     x = seq[-1] + seq[-2] # <-- does the adding for the current number and the one prior
     seq.append(x)
 
--- weightConverter.py
+++ weightConverter.py
@@ -2,14 +2,14 @@
 def weightConvert():
 
     weight = int(input('weight value: '))
-    metric = input('(k)g or (l)bs')
+    metric = input('(k)g or (l)bs: ')
     poundMulti = 2.20462
     kgMulti = 0.453592
 
     if metric.lower() == 'k':
-        print(weight * poundMulti)
+        print(weight * poundMulti) # <--- converts to pounds
     elif metric.lower() == 'l':
-        print(weight * kgMulti)
+        print(weight * kgMulti) # <--- converts to kilograms
     else:
         weightConvert()
 
